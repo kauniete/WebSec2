09a89a1dc55535dda0ee53e8e8e04d00bd82dbe1
karolinakv <karolinavalentova2@gmail.com>
2021-05-06 13:26:09 +0200

    improved the login attempts functionality

--- api/login-action.php
+++ api/login-action.php
@@ -1,5 +1,4 @@
 <?php
-
 $username = $_POST['username'];
 $password = $_POST['password'];
 
@@ -8,86 +7,127 @@ if(! (isset($password)) ) { sendError(400, 'Missing username or password', __LIN
 if( strlen($_POST['username']) > 50 ){ sendError(400, 'Username cannot be longer than 50 characters', __LINE__); }
 if( strlen($_POST['password']) > 50 ){ sendError(400, 'Password cannot be longer than 50 characters', __LINE__); }
 
-require_once (__DIR__.'/../private/db.php');
+$dbHandler = require_once (__DIR__.'/../private/db.php');
 
-try {
-    // check if the credentials exist
-    $q = $db->prepare("
+class LoginHandler {
+    static function doGetUserByUsername($db, $userUserName) {
+        $q = $db->prepare("
         SELECT *
         FROM users
-        WHERE users.username = :username LIMIT 1
+        WHERE users.userUserName = :userUserName LIMIT 1
         ");
-    $q->bindValue(':username', $_POST['username']);
-    $q->execute();
-    $aRow = $q->fetchAll();
+        $q->bindValue(':userUserName', $userUserName);
+        $q->execute();
+        $aRow = $q->fetchAll();
+        if(is_array($aRow) && count($aRow)) {
+            return $aRow[0];
+        } else {
+            return null;
+        }
+    }
+
+    static function doGetLoginActivity($db, $userUserName) {
+        /** @var PDO $db */
+        $q = $db->prepare("
+        SELECT *
+        FROM logtry
+        WHERE logtry.userName = :userName LIMIT 1
+        ");
+        $q->bindValue(':userName', $userUserName);
+        $q->execute();
+        $aRow = $q->fetchAll();
+        if(is_array($aRow) && count($aRow)) {
+            return $aRow[0];
+        } else {
+            return null;
+        }
+    }
+
+    static function doUnblockUser($db) {
+        $tmpDate = new DateTime();
+        $now = date ('Y-m-d H:i:s', $tmpDate->getTimestamp());
+        /** @var PDO $db */
+        $q = $db->prepare("
+                    UPDATE logtry
+                    SET logtry.logCount = 0, logtry.lastLog = :timeNow
+                    WHERE logtry.userName = :userName LIMIT 1
+                ");
+        $q->bindValue(':userName', $_POST['username']);
+        $q->bindValue(':timeNow', $now);
+        $q->execute();
+    }
+    static function doIncrementLoginAttempt($db, $userUserName) {
+        $tmpDate = new DateTime();
+        $now = date ('Y-m-d H:i:s', $tmpDate->getTimestamp());
+        /** @var PDO $db */
+        $q = $db->prepare("
+                UPDATE logtry
+                SET logtry.logCount = logtry.logCount + 1, logtry.lastLog = :timeNow
+                WHERE logtry.userName = :userName LIMIT 1
+            ");
+        $q->bindValue(':userName', $userUserName);
+        $q->bindValue(':timeNow', $now);
+        $q->execute();
+    }
+    static function doCreateLoginActivity($db, $userId) {
+        $tmpDate = new DateTime();
+        $now = date ('Y-m-d H:i:s', $tmpDate->getTimestamp());
+        /** @var PDO $db */
+        $q = $db->prepare("
+                INSERT INTO loggs VALUES(null, :userId, 0, :timeNow);
+            ");
+        $q->bindValue(':userId', $userId);
+        $q->bindValue(':timeNow', $now);
+        $q->execute();
+    }
+}
+try {
+    // check if the user exists
+    $currentUser = LoginHandler::doGetUserByUsername($dbHandler, $_POST['username']);
+    if($currentUser === null) {
+        header('Content-Type: application/json');
+        sendError(400, 'Invalid email or password', __LINE__);
+        return;
+    }
+
+    $logActivityResults = LoginHandler::doGetLoginActivity($dbHandler, $currentUser->userUserName);
+
+    // If this user never logged in an activity log entry is created
+    if($logActivityResults === null) {
+        LoginHandler::doCreateLoginActivity($dbHandler, $currentUser->userId);
+    } else if (intval($logActivityResults->logCount) >= 3) {
+        $lastLoginAttemptStr = $logActivityResults->lastLog;
+        $lastLoginAttempt = new DateTime($lastLoginAttemptStr);
+
+        // check if 5 minutes passed since last attempt
+        if(!doCheckTimeDiff($lastLoginAttempt)) {
+            // send error that the user must wait 5 minutes
+            header('Content-Type: application/json');
+            sendError(400, 'You have to wait ~5 min, too many failed login attempts', __LINE__);
+            return;
+        } else {
+            LoginHandler::doUnblockUser($dbHandler);
+        }
+    }
 
     // hash pepper to the password so it could match
     $aData = json_decode(file_get_contents(__DIR__.'./../private/data.txt'));
     $pepper = $aData[0]->key;
     $pwd = $_POST['password'];
     $pwd_peppered = hash_hmac("sha256", $pwd, $pepper);
-    $pwd_hashed = $aRow[0]->password;
+    $pwd_hashed = $currentUser->userPassword;
 
+    //If current user password and posted password do not match
     if(! password_verify($pwd_peppered, $pwd_hashed) ) {
-        // check if the login failed 3 times
-        $q = $db->prepare("
-            SELECT loginattempts.attempts, loginattempts.lastLogin
-            FROM users
-            INNER JOIN loginattempts ON users.id = loginattempts.user_fk
-            WHERE users.username = :username AND loginattempts.attempts = 3
-        ");
-        $q->bindValue(':username', $_POST['username']);
-        $q->execute();
-        $aRow = $q->fetchAll();
-
-        if($q->rowCount() === 1) {
-            // check if the login failed 3 times and 5 minutes passed since last attempt
-            $lastLoginAttemptStr = $aRow[0]->lastLogin;
-            $lastLoginAttempt = new DateTime($lastLoginAttemptStr);
-
-            if(!doCheckTimeDiff($lastLoginAttempt)) {
-                // send error that the user must wait 5 minutes
-                header('Content-Type: application/json');
-                sendError(400, 'You have to wait ~5 min, too many failed login attempts', __LINE__);
-                return;
-            } else {
-                // if 5 minutes passed since the last update, reset the login attempts
-                $tmpDate = new DateTime();
-                $now = date ('Y-m-d H:i:s', $tmpDate->getTimestamp());
-                $q = $db->prepare("
-                    UPDATE users
-                    INNER JOIN loginattempts ON users.id = loginattempts.user_fk
-                    SET loginattempts.attempts = 0, loginattempts.lastLogin = :timeNow
-                    WHERE users.username = :username LIMIT 1
-                ");
-                $q->bindValue(':username', $_POST['username']);
-                $q->bindValue(':timeNow', $now);
-                $q->execute();
-            }
-        } else {
-            // if login fails, add to the user a failed login attempt
-            $tmpDate = new DateTime();
-            $now = date ('Y-m-d H:i:s', $tmpDate->getTimestamp());
-            $q = $db->prepare("
-                UPDATE users
-                INNER JOIN loginattempts ON users.id = loginattempts.user_fk
-                SET loginattempts.attempts = loginattempts.attempts + 1, loginattempts.lastLogin = :timeNow
-                WHERE users.username = :username LIMIT 1
-            ");
-            $q->bindValue(':username', $_POST['username']);
-            $q->bindValue(':timeNow', $now);
-            $q->execute();
-            header('Content-Type: application/json');
-            echo json_encode($q->rowCount());
-        }
-
+        LoginHandler::doIncrementLoginAttempt($dbHandler, $currentUser->userUserName);
+        header('Content-Type: application/json');
         sendError(400, 'Invalid email or password', __LINE__);
     }
 
     session_start();
-    $_SESSION['userId'] = $aRow[0]->id;
-    $_SESSION['username'] = $aRow[0]->username;
-    $_SESSION['password'] = $aRow[0]->password;
+    $_SESSION['userId'] = $currentUser->userId;
+    $_SESSION['username'] = $currentUser->userUserName;
+    $_SESSION['password'] = $currentUser->userPassword;
 
     http_response_code(200); // default is this line
     header('Content-Type: application/json');
@@ -109,7 +149,7 @@ function sendError($iErrorCode, $sMessage, $iLine){
 }
 
 function doCheckTimeDiff(DateTime $dateTime) {
-    $secondDate = new \DateTime();
+    $secondDate = new DateTime();
 
     $diff = $dateTime->diff($secondDate);
 
