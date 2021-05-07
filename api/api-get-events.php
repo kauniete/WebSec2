4b0ffe0db4008b0684a3064c00da646c12a66952
Beata JoKo <beat1078@stud.kea.dk>
2021-05-07 22:38:33 +0200

    mergin branches

---
+++ api/api-get-events.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once(__DIR__.'/../private/db.php');
+
+try{
+    
+    
+  
+  }catch (Exception $ex) {
+    header('Content-Type: application/json');
+    echo '{"message":"error '.$ex.'"}';
+  }
