a42d514cad00c69d565ed84995e0315c4042a26e
Beata JoKo <beat1078@stud.kea.dk>
2021-05-07 23:29:58 +0200

    fix merge

---
+++ private/db.php
@@ -0,0 +1,34 @@
+<?php
+try{
+  $dbUserName = 'root';
+  $dbPassword = ''; // root | admin
+  $dbConnection = 'mysql:host=localhost; dbname=websec; charset=utf8mb4';
+  $options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // try-catch
+    // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC 
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+    // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_NUM // [[],[],[]]
+  ];
+    return new PDO(  $dbConnection,
+                    $dbUserName,
+                    $dbPassword ,
+                    $options );
+}catch(PDOException $ex){
+  echo $ex;
+  echo 'error';
+  exit();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
