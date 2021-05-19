9e6d6ef90587b25b26cfbeb70ec54d4f34f4cdb4
kauniete <38038319+kauniete@users.noreply.github.com>
2021-05-19 14:21:28 +0200

    Update routes.php

--- routes.php
+++ routes.php
@@ -5,7 +5,9 @@ require_once(__DIR__.'/router.php');
 // #####################################
 // #####################################
 // #####################################
-get('/login', 'index.php');
+//get('/login', '/index.php');
+//altering the below line so it also routes to the root yellowmellow.indraja.dk, not only yellowmellow.indraja.dk/index.php
+get('/', 'index.php');
 get('/signup', '/verify-user.php');
 get('/home', '/home.php');
 
