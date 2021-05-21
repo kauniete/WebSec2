c833d3adc66e7f715cf2143eb3e6f4be7c59957b
kauniete <38038319+kauniete@users.noreply.github.com>
2021-05-21 22:10:35 +0200

    Update verify-user.php

--- verify-user.php
+++ verify-user.php
@@ -6,8 +6,8 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
-    <title>Verify account</title>
+     <title>Verify account</title>
+     <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
     <!-- Social media CDN -->
      <!--<script src='https://kit.fontawesome.com/a076d05399.js'></script>
     <!-- favicon icon -->
